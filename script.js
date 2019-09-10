34389f67178762ee85d310e43253d3a5a924bbdf
terencecistudent <terencelogue100@hotmail.co.uk>
2019-09-10 16:13:53 +0000

    inital commit

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $("#stream1_btn").click(function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream1").addClass('highlight_stream');
+   });
+   $("#stream2_btn").on("click", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream2").addClass('highlight_stream');
+   });
+   $("#stream3_btn").on("click", function() {
+        $(".stream1").removeClass('highlight_stream');
+        $(".stream2").removeClass('highlight_stream');
+        $(".stream3").removeClass('highlight_stream');
+        $(".stream3").addClass('highlight_stream');
+   });
+
+   // Challenges
+
+   $("p").on("click", function() {
+       $("p").css("color", "red");
+   });
+
+   $("h2").mouseenter(function() {
+
+   })
+
+});
