3ff38052ebdbdc6c5f1c42f4db7e3b8b9135e7aa
terencecistudent <terencelogue100@hotmail.co.uk>
2019-09-12 15:03:18 +0000

    final commit

--- script.js
+++ script.js
@@ -27,4 +27,13 @@ $(document).ready(function() {
    $("p").css("color", "blue").slideUp(2000).slideDown(2000);
    $("a").attr("href", "http://www.example.com");
 
+
+   $(".stream-nav").on("click", function() {
+       var allStreamsCardsSelector = ".card";
+       var allStreamCardsSelector = "." + this.id + "-card";
+
+       $("allStreamsCardsSelector").removeClass("card-highlight");
+       $("allStreamCardsSelector").addClass("card-highlight");
+   })
+
 });
