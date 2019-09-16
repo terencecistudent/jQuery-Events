6b163e019e8e92bd6184e127d7abef1d50922f18
terencecistudent <terencelogue100@hotmail.co.uk>
2019-09-16 10:29:49 +0000

    another final commit

--- js/traverse.js
+++ js/traverse.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
-    $("p").click(function() {
-        $("p").children("a").css("background-color", "yellow");
-    });
+	//sets <a> element within paragraph  to yellow
+	$("p").click(function(){
+		$(this).children("a").css("background-color", "yellow"); /* returns all the <a> child elements that are
+		within this paragraph*/
+	});
+
+	// Toggle the visibility of the paragraph when a button is clicked
+	$("button").click(function(){
+		$(this).prev().slideToggle('slow');
+	});
+
+    /*$("button").click(function() {
+        $(this).slideDown(1000);
+    });*/
+
+    $("img").click(function() {
+		$(this).next().children("p").slideDown();
+	});
+
+    $(".card").click(function(){
+        $(this).toggleClass("hightlight");
+    })
 });
